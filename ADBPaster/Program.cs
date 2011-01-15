4bb90dd4da7567b477bec6cf7dfd98d39cd4da30
frostzhenya <sexplay2009@mail.ru>
2011-01-15 21:38:04 +0300

    instal

---
+++ ADBPaster/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ADBParser
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string adbFolderName = "adb";
+            if (!Directory.Exists(adbFolderName))
+            {
+                Console.WriteLine("adb folder does not exist!");
+                return;
+            }
+            var adbParser = new ADBParser(adbFolderName);
+        }
+    }
+}
